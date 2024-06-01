93eec45c88593a03b2dc4aad57df0e8c705ae3a9
XerOssa <jacek.ossowski95.@gmail.com>
2024-06-01 20:32:26 +0200

    cleaning

--- sciaga.py
+++
@@ -1,5 +0,0 @@
-# python manage.py runserver              uruchomienie serwera
-# python manage.py startapp newapp        założenie nowej aplikacji
-# python manage.py makemigrations         Wygenerowanie migracji na podstawie zmian w modelach
-# python manage.py migrate         Zaaplikowanie wygenerowanej migracji do bazy danych
-# cd poker/djangoenv/scripts/activate
