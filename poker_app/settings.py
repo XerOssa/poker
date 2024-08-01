39f6504c63a05952f9e6a655630876bbaf6400d6
XerOssa <jacek.ossowski95.@gmail.com>
2024-08-01 22:56:59 +0200

    settings

--- poker_app/settings.py
+++ poker_app/settings.py
@@ -102,7 +102,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "djangoenv/Lib/site-packages/django_browser_reload/static"),
+    ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 MEDIA_URL = '/media/'
